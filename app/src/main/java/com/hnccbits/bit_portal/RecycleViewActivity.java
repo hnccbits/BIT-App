303d404d4c109146a189d842048d59ce4b5e2e9c
Anubhaw19 <anubhawsharma99@gmail.com>
2020-03-14 23:52:49 +0530

    recycleview added

---
+++ app/src/main/java/com/hnccbits/bit_portal/RecycleViewActivity.java
@@ -0,0 +1,14 @@
+package com.hnccbits.bit_portal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecycleViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycle_view);
+    }
+}

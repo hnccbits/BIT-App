12dbdd3f7d38d3e143f5eadd4f3089b1cac8d2f3
Anubhaw19 <anubhawsharma99@gmail.com>
2020-03-15 01:15:36 +0530

    ArrayAdapter code added

--- app/src/main/java/com/hnccbits/bit_portal/Data.java
+++ app/src/main/java/com/hnccbits/bit_portal/Data.java
@@ -1,4 +1,21 @@
 package com.hnccbits.bit_portal;
 
 class Data {
+        private  String mDataText;
+        private int mDataImgRecourceId;
+
+        public Data(String text,int resourceId)
+        {
+            mDataText=text;
+            mDataImgRecourceId=resourceId;
+        }
+        public String getDataText()
+        {
+            return mDataText;
+        }
+        public int getmDataImgRecourceId()
+        {
+            return mDataImgRecourceId;
+        }
+
 }
--- app/src/main/java/com/hnccbits/bit_portal/QueryUtils.java
+++ app/src/main/java/com/hnccbits/bit_portal/QueryUtils.java
@@ -2,9 +2,21 @@ package com.hnccbits.bit_portal;
 
 import java.util.ArrayList;
 
+/**
+ * this class will just  take the data in JSON format ,extract the desired result and add it to the array list.
+ */
 class QueryUtils {
+
+    //TODO : JSON data input here...,
+
+
+    private QueryUtils() { } // a private constructor ,so that its object can't be created.
+
     public static ArrayList<Data> ExtractData() {
 
+// TODO :  JSON parsing will be done here...+ adding the result into ArrayList
 
+        ArrayList<Data> data= new ArrayList<Data>();
+        return data;
     }
 }

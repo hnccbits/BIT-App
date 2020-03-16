862289f4dbbca56df8a16b86af65b9a435245912
Shivam Anand <anand.shivam44@yahoo.com>
2020-03-16 21:09:50 +0530

    revert

--- app/src/main/java/com/hnccbits/bit_portal/show_logo.java
+++ app/src/main/java/com/hnccbits/bit_portal/show_logo.java
@@ -2,13 +2,23 @@ package com.hnccbits.bit_portal;
 import com.hnccbits.bit_portal.R;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.Timer;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class show_logo extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_show_logo);
+
+        startActivity(new Intent(show_logo.this, StartActivity.class));
+
+
     }
 }
